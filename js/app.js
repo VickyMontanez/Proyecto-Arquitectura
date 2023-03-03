4455ed55ede2ddb8a955b173869896a4eff83f27
JuanDavidAvilaRaveloCampus <juanavila8856@gmail.com>
2023-03-03 04:03:13 -0500

    nimodo

--- js/app.js
+++ js/app.js
@@ -11,3 +11,21 @@ const sectionpage = new fullpage ('#fullpage',{
     menu:'#menu',
     anchors:['whoAre','contact','briefCase','services','create'],
 });
+
+
+// const Typed = new Typed('.typed', {
+// 	strings: ['PORTAFOLIO'],
+
+// 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+// 	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+// 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+// 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+// 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+// 	shuffle: false, // Alterar el orden en el que escribe las palabras.
+// 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+// 	loop: false, // Repetir el array de strings
+// 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+// 	showCursor: true, // Mostrar cursor palpitanto
+// 	cursorChar: '|', // Caracter para el cursor
+// 	contentType: 'html', // 'html' o 'null' para texto sin formato
+// });
